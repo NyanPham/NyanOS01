1cfd2682af2d7ae579c7a7dbd14fabab2006cb95
phamthanhnhanussh@gmail.com <phamthanhnhanussh@gmail.com>
2025-10-13 18:11:04 +0700

    Layout the project with the top level including bootloader and kernel
    Added makefiles and .gdbinit to automate the build and debug process

--- NyanOS/bootloader/bootloader.asm
+++ NyanOS/bootloader/bootloader.asm
@@ -4,7 +4,7 @@
 ;********************************************
 org 0x7c00
 bits 16
-starts: jmp boot
+start: jmp boot
 
 ;; constant and variable definitions
 msg db "Welcome to NyanOS!", 0ah, 0dh, 0h
@@ -12,7 +12,7 @@ msg db "Welcome to NyanOS!", 0ah, 0dh, 0h
 boot:
 	cli ; no interrupts
 	cld ; all that we need to init
-   
+
     mov bh, 14
     mov bl, 24
     call MovCursor
@@ -20,6 +20,23 @@ boot:
     mov si, msg
     call Print
 
+    mov ax, 0x50
+
+    ;; set the buffer at es:bx (0x50:0x0)
+    mov es, ax  
+    xor bx, bx
+    
+    mov al, 2   ; read 2 sectors
+    mov ch, 0   ; track 0
+    mov cl, 2   ; sector to start reading (from the second sector)
+    mov dh, 0   ; head number
+    mov dl, 0   ; drive number
+
+    mov ah, 0x02    ;read sectors from disk 
+    int 0x13    ; call the BIOS routine
+    jmp 0x50:0x0    ; jump and execute the sector!
+
+    
 	hlt ; halt the system
 
 %include "io.asm"
